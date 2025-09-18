18863ecfd6f8507c655e07fa03305335b9223410
Jan Elia <jan.elia995@hotmail.com>
2025-09-18 03:52:09 +0200

    Installing icons and adding a toggle theme function

--- components/providers.tsx
+++ components/providers.tsx
@@ -4,8 +4,13 @@ import { ThemeProvider, useTheme } from 'next-themes'
 
 export default function Providers({children}: {children: React.ReactNode}){
     return(
-        <ThemeProvider attribute="class">
-            {children}
+        <ThemeProvider
+        enableSystem 
+        attribute='class'
+        defaultTheme='system'
+        disableTransitionOnChange
+        >
+         {children}
         </ThemeProvider>
     )
 }
